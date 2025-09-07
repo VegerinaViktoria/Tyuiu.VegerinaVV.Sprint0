985b53ed62a9640d1be0b7aa7ef1928b016f8f7b
Tyuiu.VegerinaVV@outlook.com <vvviktoriavvvegerina@gmail.com>
2025-09-07 20:53:23 +0500

    Task2

---
+++ Tyuiu.VegerinaVV.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿using Tyuiu.VegerinaVV.Sprint0.Task2.V0.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.GetMessage("Игорь"));
+        Console.ReadKey();
+    }
+}
