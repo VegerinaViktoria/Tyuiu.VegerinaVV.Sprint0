49452cf3bc69c5e71b46b47a0fe76f48edaf9694
VegerinaViktoria <vvviktoriavvvegerina@gmail.com>
2025-09-08 11:12:06 +0500

    Task1

--- Tyuiu.VegerinaVV.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.VegerinaVV.Sprint0.Task1.V0/Program.cs
@@ -3,8 +3,8 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        string name = "Игорь";
-        int age = 45;
+        string name = "Вика";
+        int age = 18;
         Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
         Console.ReadKey();
     }
