8c352e53f1e02e397db7f561f0222319134d5ddf
Tyuiu.VegerinaVV@outlook.com <vvviktoriavvvegerina@gmail.com>
2025-09-10 21:02:30 +0500

    Task5

---
+++ Tyuiu.VegerinaVV.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.VegerinaVV.Sprint0.Task5.V0.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("A + B = " + DataService.Addition(1, 5));
+        Console.WriteLine("A - B = " + DataService.Subtraction(15, 5));
+        Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
+        Console.WriteLine("A / B = " + DataService.Division(5, 0));
+        Console.ReadKey();
+    }
+}
